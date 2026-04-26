2edd0873673b2b8ecb73cfd918d1d7d0cf60fcda
xew <maxencelupion@gmail.com>
2026-04-26 19:57:40 +0200

    add(middlewares): error & logger middlewares added

---
+++ backend/src/middlewares/error.middleware.ts
@@ -0,0 +1,28 @@
+import { Request, Response, NextFunction } from 'express';
+
+export class AppError extends Error {
+    constructor(
+        public message: string,
+        public statusCode: number = 500,
+        public provided?: JSON,
+    ) {
+        super(message);
+        this.name = 'AppError';
+    }
+}
+
+export const errorMiddleware = (
+    err: Error,
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) => {
+    console.error(err);
+
+    if (err instanceof AppError) {
+        res.status(err.statusCode).json({ message: err.message, provided: err.provided });
+        return;
+    }
+
+    res.status(500).json({ message: 'Internal server error' });
+};
