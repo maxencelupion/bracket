83c87b4a9b2a2bb6e1ac6d0eb2713387ba3fd049
xew <maxencelupion@gmail.com>
2026-04-26 21:27:35 +0200

    add(validation middleware): validation middleware added

---
+++ backend/src/middlewares/validate.middleware.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from 'express';
+import { z } from 'zod';
+import logger from '../config/logger';
+
+export const validate =
+  (schema: z.ZodType) => (req: Request, res: Response, next: NextFunction) => {
+    const result = schema.safeParse(req.body);
+
+    if (!result.success) {
+      logger.warn({ errors: z.treeifyError(result.error) }, 'Validation error');
+
+      res.status(400).json({
+        message: 'Validation error',
+        error: z.treeifyError(result.error),
+      });
+
+      return;
+    }
+
+    req.body = result.data;
+    next();
+  };
