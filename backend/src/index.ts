1b8f6dfb8738aab0a4d7e04bc9983f0108a1588a
xew <maxencelupion@gmail.com>
2026-04-26 19:57:55 +0200

    add(index): index file added

---
+++ backend/src/index.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import { createServer } from 'http';
+import cors from 'cors';
+import 'dotenv/config';
+import { errorMiddleware } from './middlewares/error.middleware';
+import { loggerMiddleware } from './middlewares/logger.middleware';
+import logger from './config/logger';
+
+const app = express();
+const httpServer = createServer(app);
+
+app.use(cors());
+app.use(express.json());
+
+app.use(loggerMiddleware);
+app.use(errorMiddleware);
+
+const PORT = process.env.PORT ?? 4000;
+httpServer.listen(PORT, () => {
+  logger.info(`Server running on port http://localhost:${PORT}`);
+});
