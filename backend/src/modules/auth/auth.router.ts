ec2dc8c15c228cd89bff6b3e48545bc769955369
xew <maxencelupion@gmail.com>
2026-04-29 20:37:38 +0200

    fix(auth controller): imports cleaned

--- backend/src/modules/auth/auth.router.ts
+++ backend/src/modules/auth/auth.router.ts
@@ -1,19 +1,14 @@
 import { Router } from 'express';
 import { validate } from '../../middlewares/validate.middleware.js';
 import { registerDto, loginDto, editDto } from './auth.dto.js';
-import {
-  registerController,
-  loginController,
-  editController,
-  profileController,
-} from './auth.controller.js';
+import * as authController from './auth.controller.js';
 import { authMiddleware } from '../../middlewares/auth.middleware.js';
 
 const router = Router();
 
-router.post('/register', validate(registerDto), registerController);
-router.post('/login', validate(loginDto), loginController);
-router.patch('/profile', authMiddleware, validate(editDto), editController);
-router.get('/profile', authMiddleware, profileController);
+router.post('/register', validate(registerDto), authController.registerController);
+router.post('/login', validate(loginDto), authController.loginController);
+router.patch('/profile', authMiddleware, validate(editDto), authController.editController);
+router.get('/profile', authMiddleware, authController.profileController);
 
 export default router;
