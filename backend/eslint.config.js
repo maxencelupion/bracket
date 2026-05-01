d6f05b2bf2f74f88af527b60ab148c057ba9f561
xew <maxencelupion@gmail.com>
2026-05-01 13:13:45 +0200

    fix(eslint config): esling config file fixed

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,13 +1,17 @@
 import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
 
 export default defineConfig([
+  ...tseslint.configs.recommended,
   {
+    files: ['src/**/*.ts'],
     rules: {
       semi: 'error',
       'prefer-const': 'error',
       '@typescript-eslint/no-unused-vars': 'warn',
       '@typescript-eslint/explicit-function-return-type': 'off',
       '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-namespace': "off"
     },
   },
 ]);
