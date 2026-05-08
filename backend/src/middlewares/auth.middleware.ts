08a85d3daa5b21fad3f1138bfe00e83fcbd407a0
xew <maxencelupion@gmail.com>
2026-05-08 11:48:19 +0200

    upd(auth middleware): 2 functions added to perform permission validations on upcoming match module

--- backend/src/middlewares/auth.middleware.ts
+++ backend/src/middlewares/auth.middleware.ts
@@ -2,6 +2,7 @@ import type { NextFunction, Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
 import { AppError } from './error.middleware.js';
 import { env } from '../config/env.js';
+import prisma from '../config/prisma.js';
 
 export interface JwtPayload {
   userId: string;
@@ -33,5 +34,66 @@ export const authMiddleware = (req: Request, res: Response, next: NextFunction) 
     next();
   } catch {
     throw new AppError('Invalid or expired token', 401);
+  }
+};
+
+export const isBracketOwner = async (
+  req: Request<{ bracketId: string }>,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const bracket = await prisma.bracket.findUnique({
+      where: { id: req.params.bracketId },
+      select: { ownerId: true },
+    });
+
+    if (!bracket) {
+      return next(new AppError('Bracket not found', 404));
+    }
+
+    if (bracket.ownerId !== req.user!.userId) {
+      return next(new AppError('Forbidden', 403));
+    }
+
+    next();
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const isMatchParticipantOrOwner = async (
+  req: Request<{ bracketId: string; matchId: string }>,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const match = await prisma.match.findUnique({
+      where: { id: req.params.matchId },
+      select: {
+        bracket: { select: { ownerId: true } },
+        matchParticipants: { select: { playerId: true } },
+      },
+    });
+
+    if (!match) {
+      return next(new AppError('Match not found', 404));
+    }
+    const userId = req.user!.userId;
+    const isOwner = match.bracket.ownerId === userId;
+
+    if (isOwner) {
+      next();
+    }
+
+    const isParticipant = match.matchParticipants.some((p) => p.playerId === userId);
+
+    if (isParticipant) {
+      next();
+    }
+
+    return next(new AppError('Forbidden', 403));
+  } catch (err) {
+    next(err);
   }
 };
